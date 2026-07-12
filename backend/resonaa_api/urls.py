e6e3290efbbb17dbb6125a110c695d86c0770eae
Nsaini0622 <nsaini0622@gmail.com>
2026-07-12 16:11:31 +0530

    feat: create user signup api with mongoDB validation

--- backend/resonaa_api/urls.py
+++ backend/resonaa_api/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from users import views as user_views
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/health/', views.health_check, name='health_check'),
+    path('api/users/signup/', user_views.signup, name='signup'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSignupSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def signup(request):
+    # Frontend data send to serializer
+    serializer = UserSignupSerializer(data=request.data)
+    
+    # if data is correct (validation pass)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response({
+            "message": "User registered successfully!",
+            "username": user.username
+        }, status=status.HTTP_201_CREATED)
+    
+    # if data is wrong (eg. email already exists)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
